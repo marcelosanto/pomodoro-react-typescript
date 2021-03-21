480e7a142f1bb5362932e7c9bd5327d67ad7e80c
Marcelo Santos <rocha.mer21@gmail.com>
2021-03-21 16:54:13 -0300

    feat: button work and pause

--- src/utils/seconds-to-time.ts
+++ src/utils/seconds-to-time.ts
@@ -3,5 +3,5 @@ export function secondsToTime(seconds: number): string {
   const minutos = zeroLeft(seconds / 60)
   const segundos = zeroLeft(seconds % 60)
 
-  return `${minutos}:${segundos}s`
+  return `${minutos}:${segundos}`
 }
